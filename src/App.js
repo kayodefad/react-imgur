440543e32d60cfe7b309e7ab45c164364f8a023e
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-07-11 20:12:12 +0100

    Initial commit that can display and paginate through imgur

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
 import React, { Component } from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import Image from './Image';
+import { searchGallery } from './services/imgur';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {images: [], page: 0};
+  }
+
+  loadImages(page) {
+    searchGallery(undefined, undefined, page).then((result) => {
+      this.setState({images: result.data});
+    });
+  }
+
+  prevPage() {
+    this.state.page--;
+    this.loadImages(this.state.page);
+  }
+
+  nextPage() {
+    this.state.page++;
+    this.loadImages(this.state.page);
+  }
+
+  componentDidMount() {
+    this.loadImages();
+  }
+
   render() {
+    var containerStyle = {
+      paddingTop: '70px'
+    };
+
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <Navbar brand='React-Bootstrap' fixedTop={true}>
+          <Nav right>
+            <NavItem onClick={this.prevPage.bind(this)} disabled={this.state.page <= 0}>
+              <i className='glyphicon glyphicon-arrow-left'></i> Previous page
+            </NavItem>
+            <NavItem onClick={this.nextPage.bind(this)}>
+              Next page <i className='glyphicon glyphicon-arrow-right'></i>
+            </NavItem>
+          </Nav>
+        </Navbar>
+        <div className='container' style={containerStyle}>
+          {this.state.images.map(function(image) {
+            return (
+              <Image image={image} key={image.id}/>
+            );
+          })}
+        </div>
+      </div>
     );
   }
 }
---
+++ src/Image.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Panel } from 'react-bootstrap';
+
+export default class Image extends Component {
+
+  render() {
+    return (
+      <div>
+        <Panel header={this.props.image.title} bsStyle='primary'>
+          <img className='img-responsive center-block img-thumbnail' src={this.props.image.link} />
+        </Panel>
+      </div>
+    );
+  }
+
+}
